e8197d7c0afa88dc05a0ba676a57d0b5477f7ea7
Jeremy Walker <jwalker@lusd.net>
2023-05-09 18:26:51 +0000

    added snmp-scout-rules-example.js

---
+++ snmp-scout-rules.example.js
@@ -0,0 +1,22 @@
+const snmp = require("net-snmp"); // require net-snmp for constants
+
+module.exports = [
+    {
+        name: "Cisco Switches",
+        subnet: '10.0.0.0/8',
+        communities: ['public', 'private'], // Add more community strings if needed
+        options: {
+            timeout: 1000,
+            version: snmp.Version2c,
+        },
+        oids: [
+            '1.3.6.1.2.1.1.1.0', // sysDescr.0 (system description)
+        ],
+        matchFunction: (ip, varbinds) => {
+            let sysDescrVarbind = varbinds.find((v) => v.oid === '1.3.6.1.2.1.1.1.0');
+            sysDescr = sysDescrVarbind ? sysDescrVarbind.value.toString() : "";
+            return /Cisco IOS Software|Cisco NX-OS/.test(sysDescr);
+        },
+    },
+    // Add more SNMP rules as needed
+];
